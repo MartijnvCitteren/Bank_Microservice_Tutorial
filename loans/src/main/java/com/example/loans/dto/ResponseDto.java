1a1bff4441c6abfe1c2f01e8e6d538b764174eab
martijnvancitteren <martijnvancitteren@live.nl>
2024-07-21 18:17:12 +0200

    added loans again

---
+++ loans/src/main/java/com/example/loans/dto/ResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.loans.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Schema(name = "Response", description = "Schema to hold successful response information")
+@Builder
+public record ResponseDto(
+
+        @Schema(description = "Status code in the response")
+        String statusCode,
+
+        @Schema(description = "Status message in the response")
+        String statusMsg
+
+) {
+}
