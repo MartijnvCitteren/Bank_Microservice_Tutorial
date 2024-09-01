ff22e5d1e7275a82fa0aa747b075e948dc818625
martijnvancitteren <martijnvancitteren@live.nl>
2024-09-01 15:55:13 +0200

    added getter and setter for contactinfo dto's

--- cards/src/main/java/com/example/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/example/cards/dto/CardsContactInfoDto.java
@@ -1,11 +1,15 @@
 package com.example.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix="cards")
+@Getter
+@Setter
 public class CardsContactInfoDto {
     String message;
     Map<String, String> contactDetails;
